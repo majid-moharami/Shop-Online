71d2d3f0f4a1a38da16d2fd94b3040d73b6ace87
majid-moharami <majid.moharami79@gmail.com>
2020-12-21 21:11:29 +0330

    create loading place holder for HomeFragment using MutableLiveData<State> and when all 3 list of fragment fetched this live data will set value and product show to user. define a boolean = true and when loading finished it will be false that mine dont need to show loading.

--- app/src/main/java/com/example/digikala/data/repository/ProductRepository.java
+++ app/src/main/java/com/example/digikala/data/repository/ProductRepository.java
@@ -9,6 +9,7 @@ import com.example.digikala.data.network.parameter.RequestParams;
 import com.example.digikala.data.network.retrofit.RetrofitInstance;
 import com.example.digikala.data.network.retrofit.WooCommerceService;
 import com.example.digikala.utillity.ListType;
+import com.example.digikala.utillity.State;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,21 +22,26 @@ import retrofit2.Retrofit;
 public class ProductRepository {
     private static ProductRepository sProductRepository;
     private WooCommerceService mCommerceService;
-    private Retrofit mRetrofit = RetrofitInstance.getInstance();
-    private Retrofit mRetrofitCategoryProducts = RetrofitInstance.getProductsOfCategory();
-    private Retrofit mRetrofitSingleProduct = RetrofitInstance.getInstanceSingleProduct();
-    private Retrofit mRetrofitSearchProduct = RetrofitInstance.getProductsOfSearch();
+    private final Retrofit mRetrofit = RetrofitInstance.getInstance();
+    private final Retrofit mRetrofitCategoryProducts = RetrofitInstance.getProductsOfCategory();
+    private final Retrofit mRetrofitSingleProduct = RetrofitInstance.getInstanceSingleProduct();
+    private final Retrofit mRetrofitSearchProduct = RetrofitInstance.getProductsOfSearch();
 
-    private MutableLiveData<List<Product>> mRecentProductLiveData = new MutableLiveData<>();
-    private MutableLiveData<List<Product>> mPopularProductLiveData = new MutableLiveData<>();
-    private MutableLiveData<List<Product>> mRatingProductLiveData = new MutableLiveData<>();
-    private MutableLiveData<List<Product>> mProductCategoryLiveData = new MutableLiveData<>();
-    private MutableLiveData<List<Product>> mProductsSearchResultLiveData = new MutableLiveData<>();
-    private MutableLiveData<Product> mSingleProductLiveData = new MutableLiveData<>();
+    private final MutableLiveData<List<Product>> mRecentProductLiveData = new MutableLiveData<>();
+    private final MutableLiveData<List<Product>> mPopularProductLiveData = new MutableLiveData<>();
+    private final MutableLiveData<List<Product>> mRatingProductLiveData = new MutableLiveData<>();
+    private final MutableLiveData<List<Product>> mProductCategoryLiveData = new MutableLiveData<>();
+    private final MutableLiveData<List<Product>> mProductsSearchResultLiveData = new MutableLiveData<>();
+    private final MutableLiveData<Product> mSingleProductLiveData = new MutableLiveData<>();
 
-    private List<Product> mOldRecentProduct = new ArrayList<>();
-    private List<Product> mOldPopularProduct = new ArrayList<>();
-    private List<Product> mOldRatingProduct = new ArrayList<>();
+    private final MutableLiveData<State> mLoadingHomeFragmentStateLiveData = new MutableLiveData<>();
+
+    private final List<Product> mOldRecentProduct = new ArrayList<>();
+    private final List<Product> mOldPopularProduct = new ArrayList<>();
+    private final List<Product> mOldRatingProduct = new ArrayList<>();
+    private State mStateRecentProduct = State.LOADING;
+    private State mStateRatingProduct = State.LOADING;
+    private State mStatePopularProduct = State.LOADING;
 
     public static ProductRepository getInstance() {
         if (sProductRepository == null)
@@ -56,15 +62,17 @@ public class ProductRepository {
                 listCall.enqueue(new Callback<List<Product>>() {
                     @Override
                     public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
-                        if (response.isSuccessful()){
-                            if (response.body().size()>0){
-                                if (page == 1){
+                        if (response.isSuccessful()) {
+                            if (response.body().size() > 0) {
+                                if (page == 1) {
                                     mRecentProductLiveData.setValue(response.body());
-                                }else {
+                                } else {
                                     List<Product> list = mRecentProductLiveData.getValue();
                                     list.addAll(response.body());
                                     mRecentProductLiveData.setValue(list);
                                 }
+                                mStateRecentProduct = State.NAVIGATE;
+                                setHomeFragmentState();
                             }
                         }
                     }
@@ -80,16 +88,18 @@ public class ProductRepository {
                 listCall.enqueue(new Callback<List<Product>>() {
                     @Override
                     public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
-                        if (response.isSuccessful()){
+                        if (response.isSuccessful()) {
                             assert response.body() != null;
-                            if (response.body().size()>0){
-                                if (page == 1){
+                            if (response.body().size() > 0) {
+                                if (page == 1) {
                                     mPopularProductLiveData.setValue(response.body());
-                                }else {
+                                } else {
                                     List<Product> list = mPopularProductLiveData.getValue();
                                     list.addAll(response.body());
                                     mPopularProductLiveData.setValue(list);
                                 }
+                                mStatePopularProduct = State.NAVIGATE;
+                                setHomeFragmentState();
                             }
                         }
                     }
@@ -105,16 +115,18 @@ public class ProductRepository {
                 listCall.enqueue(new Callback<List<Product>>() {
                     @Override
                     public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
-                        if (response.isSuccessful()){
+                        if (response.isSuccessful()) {
                             assert response.body() != null;
-                            if (response.body().size()>0){
-                                if (page == 1){
+                            if (response.body().size() > 0) {
+                                if (page == 1) {
                                     mRatingProductLiveData.setValue(response.body());
-                                }else {
+                                } else {
                                     List<Product> list = mRatingProductLiveData.getValue();
                                     list.addAll(response.body());
                                     mRatingProductLiveData.setValue(list);
                                 }
+                                mStateRatingProduct = State.NAVIGATE;
+                                setHomeFragmentState();
                             }
                         }
                     }
@@ -128,18 +140,18 @@ public class ProductRepository {
         }
     }
 
-    public void fetchCategoryProduct(int categoryId , int page){
+    public void fetchCategoryProduct(int categoryId, int page) {
         mCommerceService = mRetrofitCategoryProducts.create(WooCommerceService.class);
-        Call<List<Product>> listCall = mCommerceService.productListOfCategory(RequestParams.BASE_PARAM , categoryId , page);
+        Call<List<Product>> listCall = mCommerceService.productListOfCategory(RequestParams.BASE_PARAM, categoryId, page);
         listCall.enqueue(new Callback<List<Product>>() {
             @Override
             public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     assert response.body() != null;
-                    if (response.body().size()>0){
-                        if (page == 1){
+                    if (response.body().size() > 0) {
+                        if (page == 1) {
                             mProductCategoryLiveData.setValue(response.body());
-                        }else {
+                        } else {
                             List<Product> list = mProductCategoryLiveData.getValue();
                             list.addAll(response.body());
                             mProductCategoryLiveData.setValue(list);
@@ -171,17 +183,17 @@ public class ProductRepository {
         });
     }
 
-    public void fetchSearchProducts(int page , String searchWord){
+    public void fetchSearchProducts(int page, String searchWord) {
         mCommerceService = mRetrofitSearchProduct.create(WooCommerceService.class);
         Call<List<Product>> listCall = mCommerceService.searchProducts(RequestParams.BASE_PARAM, searchWord, page);
         listCall.enqueue(new Callback<List<Product>>() {
             @Override
             public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
-                if (response.isSuccessful()){
-                    if (response.body().size()>0){
-                        if (page == 1){
+                if (response.isSuccessful()) {
+                    if (response.body().size() > 0) {
+                        if (page == 1) {
                             mProductsSearchResultLiveData.setValue(response.body());
-                        }else {
+                        } else {
                             List<Product> list = mProductsSearchResultLiveData.getValue();
                             list.addAll(response.body());
                             mProductsSearchResultLiveData.setValue(list);
@@ -197,24 +209,31 @@ public class ProductRepository {
         });
     }
 
+    private void setHomeFragmentState() {
+        if (mStateRecentProduct == State.NAVIGATE && mStatePopularProduct == State.NAVIGATE &&
+                mStateRatingProduct == State.NAVIGATE) {
+            mLoadingHomeFragmentStateLiveData.setValue(State.NAVIGATE);
+        }
+    }
+
 
     public MutableLiveData<List<Product>> getRecentProductLiveData() {
-        fetchProducts(ListType.RECENT_PRODUCT , 1);
+        fetchProducts(ListType.RECENT_PRODUCT, 1);
         return mRecentProductLiveData;
     }
 
     public MutableLiveData<List<Product>> getPopularProductLiveData() {
-        fetchProducts(ListType.POPULAR_PRODUCT , 1);
+        fetchProducts(ListType.POPULAR_PRODUCT, 1);
         return mPopularProductLiveData;
     }
 
     public MutableLiveData<List<Product>> getRatingProductLiveData() {
-        fetchProducts(ListType.RATING_PRODUCT , 1);
+        fetchProducts(ListType.RATING_PRODUCT, 1);
         return mRatingProductLiveData;
     }
 
-    public MutableLiveData<List<Product>> getProductCategoryLiveData(int id ) {
-        fetchCategoryProduct(id , 1);
+    public MutableLiveData<List<Product>> getProductCategoryLiveData(int id) {
+        fetchCategoryProduct(id, 1);
         return mProductCategoryLiveData;
     }
 
@@ -223,7 +242,7 @@ public class ProductRepository {
     }
 
     public MutableLiveData<List<Product>> getProductsSearchResultLiveData(String word) {
-        fetchSearchProducts(1 , word);
+        fetchSearchProducts(1, word);
         return mProductsSearchResultLiveData;
     }
 
@@ -249,5 +268,9 @@ public class ProductRepository {
 
     public void updateOldRatingProduct(List<Product> oldRatingProduct) {
         mOldRatingProduct.addAll(oldRatingProduct);
+    }
+
+    public MutableLiveData<State> getLoadingHomeFragmentStateLiveData() {
+        return mLoadingHomeFragmentStateLiveData;
     }
 }
